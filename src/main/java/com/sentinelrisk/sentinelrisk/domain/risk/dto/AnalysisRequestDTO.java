f2b5d16ebc925be051fc6117a5b921223bb66322
Eugenio Garcia <euugarcia94@gmail.com>
2026-02-19 15:05:40 +0100

    fix: pequeños ajustes finales de limpieza

--- src/main/java/com/sentinelrisk/sentinelrisk/domain/risk/dto/AnalysisRequestDTO.java
+++ src/main/java/com/sentinelrisk/sentinelrisk/domain/risk/dto/AnalysisRequestDTO.java
@@ -3,7 +3,7 @@ package com.sentinelrisk.sentinelrisk.domain.risk.dto;
 import jakarta.validation.constraints.NotBlank;
 
 public record AnalysisRequestDTO(
-        @NotBlank(message = "El título es obligatorio") String title, // Fíjate: una sola 't'
+        @NotBlank(message = "El título es obligatorio") String title,
         @NotBlank(message = "La descripción es obligatoria") String description
 ) {
 }
